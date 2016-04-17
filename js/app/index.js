d61a20fd126ee6783a4cb32ca1a7ce387301a619
AmirSaber Sharifi <amir@vyrent.com>
2016-04-17 17:53:19 -0400

    remove js folder

--- js/app/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var server_1 = require("./server");
-var morgan = require("morgan");
-var dotenv = require("dotenv");
-dotenv.config({ silent: true });
-var server = new server_1.Server(process.env.PORT);
-server.setMiddlewares(morgan('combined'));
-server.start();
-//# sourceMappingURL=index.js.map
